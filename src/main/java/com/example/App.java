785433c94df85c4df40e8f5d949be83c1709e8c6
Foivos Zakkak <fzakkak@redhat.com>
2023-11-22 20:47:15 +0200

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import io.vertx.pgclient.impl.codec.DataTypeCodec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( DataTypeCodec.LDT_MINUS_INFINITY );
+    }
+}
